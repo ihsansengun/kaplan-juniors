95f51c3ae821526a841e8f0692829ee7d6e24f09
Ihsan Sengun <me@ihsansengun.com>
2019-08-06 21:32:36 +0100

    first commit

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,17 @@
+import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import SmoothScroll from './modules/SmoothScroll';
+
+// import JqueryCycle from './modules/JqueryCycle';
+
+
+var mobileMenu = new MobileMenu();
+
+
+new RevealOnScroll($('.element'), '65%');
+new SmoothScroll();
+
+
+// var jqueryCycle = new JqueryCycle($('#testimonial'));
+
+
